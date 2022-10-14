456f83210d9d42f5410eb89098b51239f2362ef4
Angelillo15 <angelrizosrubios@gmail.com>
2022-10-14 20:19:54 +0200

    Changed package

--- javadocs/ram-limit-restart/package-search-index.js
+++ javadocs/ram-limit-restart/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"es.angelillo15.rlr.api"},{"l":"es.angelillo15.rlr.api.events"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"es.angelillo15.rlr.api"},{"l":"es.angelillo15.rlr.api.bukkit.events"}]
