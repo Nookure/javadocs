892e69da61c1347501ebe0eda27a2f970ec6d0e0
Angelillo15 <contact@angelillo15.es>
2024-04-27 22:53:38 +0200

    Fix build

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,7 +20,7 @@ const JavaDocCard = ({ title, image }: JavaDocCardProps) => {
       <Card isFooterBlurred radius='lg' className='border-none h-[200px] w-[400px]'>
         <Image alt={`${title} image`} className='object-fit w-full h-full' src={image} />
         <CardFooter className='flex justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10'>
-          <Button onClick={() => openInNewTab('/jd/javadocs/NookureStaff/index.html')} className='text-tiny text-white bg-black/20 px-6' variant='flat' color='default' radius='lg' size='sm'>
+          <Button onClick={() => openInNewTab('/jd/NookureStaff/index.html')} className='text-tiny text-white bg-black/20 px-6' variant='flat' color='default' radius='lg' size='sm'>
             Open in a new tab
           </Button>
         </CardFooter>
